edf65d9d9a50bff25585d34e82b1b99c35cfe84d
Agah <agahgames260@gmail.com>
2023-07-18 05:57:25 +0300

    Giga Commit for the map

--- ProjectOsadia/Assets/Scripts/Systems/HealthSystem/Shootable.cs
+++ ProjectOsadia/Assets/Scripts/Systems/HealthSystem/Shootable.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Shootable : MonoBehaviour
@@ -7,15 +9,24 @@ public class Shootable : MonoBehaviour
     [SerializeField] HealthSystemComponent health;
     [SerializeField] int hp = 100;
     private HealthSystem healthSystem;
+
+    public static event EventHandler OnShootableDead;
+    private class ShootableEventArgs : EventArgs
+    {
+        
+    }
+    
     private void Start()
     {
         healthSystem = health.GetHealthSystem();
         healthSystem.SetHealthMax(hp, true);
         healthSystem.OnDead += HealthSystem_OnDead;
     }
+    
     private void HealthSystem_OnDead(object sender, System.EventArgs e)
     {
         Debug.Log("Vurulan nesne öldü. " +  healthSystem.IsDead());
+        OnShootableDead?.Invoke(this, EventArgs.Empty);
     }
     private void OnCollisionEnter(Collision collision)
     {
